79e94cf56ccbf835529eba88f997ce42b28fd6d9
JB Nizet <jb@ninja-squad.com>
2019-02-09 17:03:44 +0100

    chore: add publication tasks (#2)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,15 +5,18 @@ plugins {
 
     java
     kotlin("jvm") version kotlinVersion
+    `maven-publish`
     id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.noarg") version kotlinVersion
     id("org.springframework.boot") version "2.1.2.RELEASE" apply false
     id("io.spring.dependency-management") version "1.0.6.RELEASE"
     id("org.jetbrains.dokka") version "0.9.17"
+    id("com.jfrog.bintray") version "1.8.4"
 }
 
-group = "com.ninjasquad"
+group = "com.ninja-squad"
 version = "0.0.1-SNAPSHOT"
+description = "MockBean and SpyBean, but for MockK instead of Mockito"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
@@ -21,6 +24,14 @@ java {
 
 repositories {
     mavenCentral()
+}
+
+val sharedManifest = Action<Manifest> {
+    attributes(
+        "Implementation-Title" to project.name,
+        "Implementation-Version" to project.version,
+        "Implementation-Vendor" to "ninja-squad.com"
+    )
 }
 
 tasks {
@@ -33,6 +44,23 @@ tasks {
 
     test {
         useJUnitPlatform()
+    }
+
+    jar {
+        manifest(sharedManifest)
+    }
+
+    register<Jar>("sourcesJar") {
+        archiveClassifier.set("sources")
+        from(sourceSets.main.get().allSource)
+        manifest(sharedManifest)
+    }
+
+    register<Jar>("javadocJar") {
+        dependsOn(javadoc)
+        archiveClassifier.set("javadoc")
+        from(javadoc.get().destinationDir)
+        manifest(sharedManifest)
     }
 }
 
@@ -58,4 +86,75 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-params")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["javadocJar"])
+
+            pom {
+                name.set(project.name)
+                description.set(project.description)
+                url.set("https://github.com/Ninja-Squad/springmockk")
+                organization {
+                    name.set("Ninja Squad")
+                    url.set("http://ninja-squad.com")
+                }
+                licenses {
+                    license {
+                        name.set("Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("jnizet")
+                        name.set("Jean-Baptiste Nizet")
+                        email.set("jb@ninja-squad.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/Ninja-Squad/springmockk")
+                    developerConnection.set("scm:git:git://github.com/Ninja-Squad/springmockk")
+                    url.set("https://github.com/Ninja-Squad/springmockk")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            url = uri("$buildDir/repo")
+        }
+    }
+}
+
+bintray {
+    user = "ninjasquad"
+    key = project.findProperty("bintray.key") as String?
+    setPublications("maven")
+    publish = true
+    pkg = PackageConfig().apply {
+        repo = "maven"
+        name = project.name
+        desc = project.description
+        websiteUrl = "https://github.com/Ninja-Squad/springmockk"
+        issueTrackerUrl = "https://github.com/Ninja-Squad/springmockk/issues"
+        vcsUrl = "https://github.com/Ninja-Squad/springmockk"
+        setLicenses("Apache-2.0")
+        version = VersionConfig().apply {
+            gpg = GpgConfig().apply {
+                sign = true
+                passphrase = project.findProperty("signing.password") as String?
+            }
+            mavenCentralSync = MavenCentralSyncConfig().apply {
+                sync = true
+                user = project.findProperty("sonatypeUsername") as String?
+                password = project.findProperty("sonatypePassword") as String?
+            }
+        }
+    }
 }
