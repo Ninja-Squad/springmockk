a67c3527ac09c8e6cb74681a1e26e2a5460ab1cb
Krzysztof Kurczewski <krzysiek.kurczewski@gmail.com>
2024-02-07 17:18:19 +0100

    Migrate from com.ninjasquad to io.mockk

--- src/main/kotlin/com/ninjasquad/springmockk/MockkFunctions.kt
+++
@@ -1,7 +0,0 @@
-package com.ninjasquad.springmockk
-
-import io.mockk.MockK
-import io.mockk.MockKGateway
-
-val <T: Any> T.isMock: Boolean
-    get() = MockK.useImpl { MockKGateway.implementation().mockFactory.isMock(this) }
